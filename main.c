429053a9bf14a29c833b0be7e680a3469906909b
Kroketagamer <alejandrogarciarosado82@gmail.com>
2025-04-04 22:03:26 +0200

    Add boolean variables

--- main.c
+++ main.c
@@ -1,61 +1,68 @@
 /*
 * Author: Alejandro Garcia
 * Date: 04-05-2025
-* Description: This program reads two cell phone specifications from files and compares them based on their display size and memory. 
-* Escribio los comentarios en ingles porque así me voy a costumbrando a escribirlos de cara al futuro.
+* Description: This program reads two cell phone specifications from files and compares them based on their display size and memory.
 */
 
 #include <stdio.h>
 #include <string.h>
-#define MAX_LENGTH 11+1                  // Maximum length of the filename   
-#define MAX_NAME 25+1                    // Maximum length of the phone name
-#define MAX_PHONES 2                     // Maximum number of phones
+#include <stdbool.h>                    // Include for bool type
+#define MAX_LENGTH 12                   // Maximum length of the filename (11 + 1 for null terminator)
+#define MAX_NAME 26                     // Maximum length of the phone name (25 + 1 for null terminator)
+#define MAX_PHONES 2                    // Maximum number of phones
 
-typedef struct {                         // Structure Tupla
+typedef struct {                        // Tupla to store phone details
     char name[MAX_NAME];
     int display;
     int memory;
 } tPhone;
 
 int main() {
-    int i;
-    tPhone phone[MAX_PHONES];
+    tPhone phones[MAX_PHONES];
     char filename[MAX_LENGTH];
-    int bestPhone;
+    int bestPhoneIndex;
+    bool displaysEqual, memoriesEqual;
+    bool fileFound = true;                 // Boolean variable to check if the file was found
     FILE *file;
 
-    for (i = 0; i < MAX_PHONES; i++) {                           // Bucle for to read two files
-        printf("FILENAME %i?\n", i + 1);         
-        scanf("%s", filename);           
+    for (int i = 0; i < MAX_PHONES; i++) {                // Loop to read phone details from files
+        printf("FILENAME %i?\n", i + 1);
+        scanf("%s", filename);
+
         file = fopen(filename, "r");
         if (file == NULL) {
-            printf("Error al abrir el archivo %s\n", filename);  // Not found file error message                                                              
-        } 
-
-        fscanf(file, "%s %d %d", phone[i].name, &phone[i].display, &phone[i].memory);
+            printf("File not found: %s\n", filename);     // Error message for missing file
+            fileFound = false;                            // Set fileFound to false                                    
+        }else { 
+        fscanf(file, "%s %d %d", phones[i].name, &phones[i].display, &phones[i].memory);
         fclose(file);
+        }
     }
+    
+    if (fileFound){                                                    // Check if both files were found    
+    displaysEqual = phones[0].display == phones[1].display;            // Bolean variables to check if displays and memories are equal
+    memoriesEqual = phones[0].memory == phones[1].memory;
 
-    if (phone[0].display == phone[1].display) {                  // Compare if displays are equal
-        if (phone[0].memory == phone[1].memory) {                // Compare if memory is equal
-            bestPhone = 1;                                       // Memory is equal, so choose the second phone                
+    if (displaysEqual) {                                 // Compare displays
+        if (memoriesEqual) {                             // Compare memories if displays are equal
+            bestPhoneIndex = 1;                          // Choose the second phone if both are equal
         } else {
-            if (phone[0].memory > phone[1].memory) {             // If memory is different, choose the one with the higher memory
-                bestPhone = 0;
+            if (phones[0].memory > phones[1].memory) {
+                bestPhoneIndex = 0;                      // Incex 0 if the first phone has more memory
             } else {
-                bestPhone = 1;
+                bestPhoneIndex = 1;                      // Index 1 if the second phone has more memory
             }
-        }            
+        }
     } else {
-        if (phone[0].display > phone[1].display) {               // If display is different, choose the one with the higher display
-            bestPhone = 0;
+        if (phones[0].display > phones[1].display) {
+            bestPhoneIndex = 0;                          // Intex 0 if the first phone has a greater display
         } else {
-            bestPhone = 1;
+            bestPhoneIndex = 1;                          // Index 1 if the second phone has a greater display
         }
     }
 
     printf("OUTPUT\n");
-    printf("THE BEST CELL PHONE IS %s\n", phone[bestPhone].name); // Print the name of the best phone using bestPhone as an index
-
+    printf("THE BEST CELL PHONE IS %s\n", phones[bestPhoneIndex].name); // Print the best phone name using the index bestPhneIndex
+    }
     return 0;
 }
